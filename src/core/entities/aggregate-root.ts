e0c8d38267e9de16728ab57470cb2b0bacb8fe13
Guilherme Viana <imluizguilara@hotmail.com>
2024-09-30 12:24:09 -0300

    stash

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,3 +1,16 @@
+import type { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domain-events'
 import { Entity } from './entity'
 
-export abstract class AggregateRoot<Props> extends Entity<Props> {}
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents() {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(event: DomainEvent) {
+    this._domainEvents.push(event)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+}
--- src/domain/forum/enterprise/entities/answer-attachments-list.ts
+++ src/domain/forum/enterprise/entities/answer-attachments-list.ts
@@ -3,6 +3,6 @@ import type { AnswerAttachment } from './answer-attachment'
 
 export class AnswerAttachmentsList extends WatchedList<AnswerAttachment> {
   compareItems(a: AnswerAttachment, b: AnswerAttachment): boolean {
-    return a.attachmentId === b.attachmentId
+    return a.attachmentId.equals(b.attachmentId)
   }
 }
